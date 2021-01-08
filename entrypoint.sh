078f1f9f7974f859e2068bf9de0701d914b45c68
rickstaa <rick.staa@outlook.com>
2021-01-08 18:12:10 +0100

    :art: Fixes bash syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -48,7 +48,7 @@ else
   # Check if remark-lint linter formatted files
   # Note: We use git since parsing wont help if the user supplied the '-s' or '-q' flags
   changed_files="$(git status --porcelain | grep .md | wc -l || echo '0')"
-  if [[ ${changed_files} -eq "0" ]]; then
+  if [[ "${changed_files}" -eq "0" ]]; then
     echo "::set-output name=is_formatted::false"
   else
     echo "::set-output name=is_formatted::true"
